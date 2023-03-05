0ac65ef81900b2f19d06c20953e58957d74adccb
Dima Lobanov <dmytro.lobanov@nure.ua>
2023-03-05 18:14:45 +0200

    fix

---
+++ bot.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const jwt_decode = require('jwt-decode');
+const json = require('json');
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+axios.defaults.baseURL = `https://back-yipq.onrender.com`;
+
+const axiosLogin = async (email, password) => {
+  const { data } = await axios.post('api/user/login', { email, password });
+
+  return jwt_decode(data.token);
+};
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('send me ur geolocation'));
+
+bot.on('message', async (ctx) => {
+  if (ctx.message?.text === 'Мой ид') {
+    const result = ctx.message?.from?.id;
+    return ctx.reply(`${result}`);
+  }
+  // const result = ctx.message?.text.split(' ')
+  // const login = result[0]
+  //const password = result[1]
+  //console.log(ctx);
+
+  // const weatherAPIUrl = `https://openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=${process.env.OWEATHER_APIKEY}`;
+  // const response = await axios.get(weatherAPIUrl);
+  //ctx.reply(`${response.data.name}: ${response.data.weather[0].main} ${response.data.main.temp} °C`);
+  // const response = await axiosLogin(login, password);
+
+  //ctx.reply(`${response.email} ${response.role} ${response.score}`);
+  console.log(ctx)
+  ctx.reply(`${ctx.message?.text}`)
+});
+
+bot.on('ochko', async (ctx) => {
+
+  console.log(ctx)
+});
+
+bot.launch();
