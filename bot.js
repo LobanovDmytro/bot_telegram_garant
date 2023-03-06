3f71336ac71c228a72f4c6d9651f1ac8454f26d7
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-06 16:32:26 +0200

    add create telegram user(new feature)

--- bot.js
+++ bot.js
@@ -1,133 +1,93 @@
-// const axios = require('axios');
-// const jwt_decode = require('jwt-decode');
-// const json = require('json');
-// require('dotenv').config();
-// const { Telegraf } = require('telegraf');
+const axios = require('axios');
+const jwt_decode = require('jwt-decode');
+const json = require('json');
+require('dotenv').config();
+const TelegramApi = require('node-telegram-bot-api');
 
-// axios.defaults.baseURL = `https://back-yipq.onrender.com`;
+axios.defaults.baseURL = `https://back-yipq.onrender.com`;
 
 // const axiosLogin = async (email, password) => {
 //   const { data } = await axios.post('api/user/login', { email, password });
-
 //   return jwt_decode(data.token);
 // };
+const axiosGetChatID = async () => {
+  const { data } = await axios.get('api/tg/get');
+  if (data.telegramUsers) {
+    return data.telegramUsers;
+  }
+};
+const axiosCreateChat = async (name, chatid, email, password) => {
+  const { data } = await axios.post('api/tg/create', { name, chatid, email, password });
+  if (data) {
+    return data;
+  }
+};
+const axiosDeleteChat = async (name, email, password) => {
+  const { data } = await axios.post('api/tg/delete', { name, email, password });
+  if (data) {
+    return data;
+  }
+};
+const token = '6149778778:AAFPlXbrTIXD_zDG4LLaeeqTH8fbKJM7kmA';
+const bot = new TelegramApi(token, { polling: true });
 
+bot.on('message', async (ctx) => {
+  const chatId = ctx.from?.id;
+  if (ctx.text === '/MyId') {
+    return bot.sendMessage(chatId, `${chatId}`);
+  }
+  if (ctx.text === 'Шо') {
+    return bot.sendMessage(chatId, "Шо ты хуесос", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Получить id", callback_data: "/MyId" }]
+        ],
+        resize_keyboard: true
+      }
+    });
+  }
 
-// const bot = new Telegraf('6116692347:AAGTOsJBqS0Jn59E6XHSlOilxmLYp4FJhug');
-
-// // bot.start((ctx) => ctx.reply('Что бы начать работу нажмите кнопку старт'));
-
-// bot.on('message', async (ctx) => {
-//   if (ctx.message?.text === '/MyId') {
-//     const result = ctx.message?.from?.id;
-//     return ctx.reply(`${result}`);
-//   }
-
-//   // const result = ctx.message?.text.split(' ')
-//   // const login = result[0]
-//   //const password = result[1]
-//   //console.log(ctx);
-
-//   // const weatherAPIUrl = `https://openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=${process.env.OWEATHER_APIKEY}`;
-//   // const response = await axios.get(weatherAPIUrl);
-//   //ctx.reply(`${response.data.name}: ${response.data.weather[0].main} ${response.data.main.temp} °C`);
-//   // const response = await axiosLogin(login, password);
-
-//   //ctx.reply(`${response.email} ${response.role} ${response.score}`);
-//   console.log(ctx)
-//   ctx.reply(`${ctx.message?.text}`)
-// });
-
-
-
-// bot.launch();
-
-const TelegramApi = require('node-telegram-bot-api');
-
-const token = '6116692347:AAGTOsJBqS0Jn59E6XHSlOilxmLYp4FJhug';
-
-const bot = new TelegramApi(token,{polling:true});
-// // axios.default.baseURL = `https://back-yipq.onrender.com`;
-
-// bot.on('message', async (ctx) => {
-//     const chatId = ctx.chat.id;
-//     bot.sendMessage(chatId, "", {
-//       reply_markup: {
-//         inline_keyboard: [
-//           [ { text: "Получить id",callback_data: "/MyId"} ]
-//         ],
-//         resize_keyboard: true
-//       }
-//     });
-
-//     bot.on('callback_query', (query) => {
-//       const chatId = query.message.chat.id;
-//       const messageId = query.message.message_id;
-//       const data = query.data;
-    
-//       if (data === "/MyId") {
-//         bot.sendMessage(chatId, `твой id чата ${chatId}`);
-//       }
-//   });
-// });
-
-
-// // Обработчик команды /start
-// bot.onText(/\/start/, async(msg) => {
-//   // Отправляем сообщение с кнопкой "Вход"
-//  await bot.sendMessage(msg.chat.id, "Для входа введите свой логин и пароль", {
-//     reply_markup: {
-//       inline_keyboard: [
-//         [{ text: "Вход" }]
-//       ],
-//       resize_keyboard: true
-//     }
-//   });
-// });
-
-// // Обработчик нажатия кнопки "Вход"
-// bot.onText(/Вход/, async(msg) => {
-//   // Отправляем сообщение с просьбой ввести логин
-//   await bot.sendMessage(msg.chat.id, "Введите логин:");
-
-//   // Ожидаем ответа пользователя с логином
-//    bot.once('message', (loginMsg) => {
-//     const login = loginMsg.text;
-
-//     // Отправляем сообщение с просьбой ввести пароль
-//   bot.sendMessage(msg.chat.id, "Введите пароль:");
-
-//     // Ожидаем ответа пользователя с паролем
-//    bot.once('message', (passwordMsg) => {
-//       const password = passwordMsg.text;
-
-//       // Отправляем сообщение с логином и паролем
-//     bot.sendMessage(msg.chat.id, `Логин: ${login}\nПароль: ${password}`);
-//     });
-//   });
-// });
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, "ИДИ НАХУЙ", {
-    reply_markup: {
-      inline_keyboard: [
-        [ { text: "Выполнить команду", callback_data: "/MyId" } ]
-      ]
-    }
-  });
+  if (ctx.text === '/start') {
+    return bot.sendMessage(chatId, "Для входа введите свой логин и пароль", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Вход", callback_data: "/login" }]
+        ],
+        resize_keyboard: true
+      }
+    });
+  }
 });
 
 bot.on('callback_query', (query) => {
-  const chatId = query.message.chat.id;
-  const messageId = query.message.message_id;
+  const chatId = query.from?.id
   const data = query.data;
-
   if (data === "/MyId") {
-    bot.sendMessage(chatId, `Выполнена команда: ${chatId}`);
+    return bot.sendMessage(chatId, `твой id чата ${chatId}`);
   }
+  if (data === "/login") {
+    bot.sendMessage(chatId, "Введите имя для записи в базу данных:");
+    return bot.once('message', async (nameMsq) => {
+      const name = nameMsq.text;
 
-  bot.answerCallbackQuery(query.id);
+      bot.sendMessage(chatId, "Введите email:");
+      bot.once('message', (loginMsg) => {
+        const login = loginMsg.text;
+
+        bot.sendMessage(chatId, "Введите пароль:");
+
+        bot.once('message', async (passwordMsg) => {
+          const password = passwordMsg.text;
+          bot.sendMessage(chatId, `Ожидайте, выполняется запрос`);
+          console.log(2, name, chatId, login, password)
+          try {
+            const response = await axiosCreateChat(name, String(chatId), login, password)
+            return bot.sendMessage(chatId, `Успешно`);
+          } catch (e) {
+            bot.sendMessage(chatId, `Пошел нахуй, ${e.response.data.message.toLowerCase()}`)
+          }
+        });
+      });
+    });
+  }
 });
-
