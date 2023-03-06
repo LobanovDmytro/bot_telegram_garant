e735341ed5919cf64bf44508373fc93419c11d4a
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-06 20:58:17 +0200

    add

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ axios.defaults.baseURL = `https://back-yipq.onrender.com`;
 const axiosGetChatID = async () => {
   const { data } = await axios.get('api/tg/get');
   if (data.telegramUsers) {
-    console.log(data.telegramUsers)
     return data.telegramUsers;
   }
 };
@@ -34,25 +33,21 @@ const axiosDeleteChat = async (name, email, password) => {
 const token = '6149778778:AAFPlXbrTIXD_zDG4LLaeeqTH8fbKJM7kmA';
 const bot = new TelegramApi(token, { polling: true });
 const buttonForm = {
-  reply_markup: JSON.stringify({
-    inline_keyboard:[
-      [ {text: 'Чаты поддержки', callback_data: '/chats'} ],
-      [ {text: 'Чаты сделок', callback_data: '/dealchats'} ],
-    ]
-  })
+  reply_markup: {
+    keyboard: [
+      [{ text: 'Чаты поддержки' }, { text: 'Чаты сделок' }, { text: 'Мой ид' }, { text: "Вход" }],
+    ],
+    resize_keyboard: true
+  }
 }
 
 bot.setMyCommands([
-  {command: '/start', description: 'Запустить или перезапустить бота'},
-  {command: '/id', description: 'Получить id'},
-  {command: '/dostup', description: 'Функционал'},
+  { command: '/start', description: 'Меню функционала' },
 ])
 
 bot.on('message', async (ctx) => {
   const chatId = ctx.from?.id;
-  if (ctx.text === '/id') {
-    return bot.sendMessage(chatId, `твой id чата ${chatId}`);
-  }
+  const data = ctx.data;
   // if (ctx.text === 'Шо') {
   //   return bot.sendMessage(chatId, "Шо ты хуесос", {
   //     reply_markup: {
@@ -62,33 +57,26 @@ bot.on('message', async (ctx) => {
   //       resize_keyboard: true
   //     }
   //   });
-  // }
-
-  if (ctx.text === '/start') {
-    return bot.sendMessage(chatId, "Для входа введите свой логин и пароль", {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: "Вход", callback_data: "/login" }]
-        ],
-        resize_keyboard: true
-      }
-    });
+  //}
+  if (ctx.text === "Мой ид") {
+    return bot.sendMessage(chatId, `Твой id чата ${chatId}`);
   }
-  if (ctx.text === '/dostup') {
+  if (ctx.text === '/start') {
     return bot.sendMessage(chatId, "Меню", buttonForm)
   }
-});
-
-
-
-
-bot.on('callback_query', (query) => {
-  const chatId = query.from?.id
-  const data = query.data;
-  if (data === "/MyId") {
-    return bot.sendMessage(chatId, `твой id чата ${chatId}`);
+  if (ctx.text === 'Чаты поддержки') {
+    bot.sendMessage(chatId, `Ожидайте, выполняется запрос`)
+    const result = await axiosGetChatID();
+    const checkAccess = result.filter(item => item.chatid === String(chatId))[0]
+    console.log('result', result, checkAccess)
+    if (checkAccess) {
+      return bot.sendMessage(chatId, `${result.reduce((acc, item) => `${acc}
+${item.name} ${item.chatid}`, '')}`)
+    } else {
+      return bot.sendMessage(chatId, `Пошел нахуй чмо без доступа`)
+    }
   }
-  if (data === "/login") {
+  if (ctx.text === "Вход") {
     bot.sendMessage(chatId, "Введите имя для записи в базу данных:");
     return bot.once('message', async (nameMsq) => {
       const name = nameMsq.text;
@@ -104,14 +92,19 @@ bot.on('callback_query', (query) => {
           bot.sendMessage(chatId, `Ожидайте, выполняется запрос`);
           try {
             const response = await axiosCreateChat(name, String(chatId), login, password)
-            axiosGetChatID()
             return bot.sendMessage(chatId, `Успешно`);
           } catch (e) {
             bot.sendMessage(chatId, `Пошел нахуй, ${e.response.data.message.toLowerCase()}`)
-            axiosGetChatID()
           }
         });
       });
     });
   }
+});
+
+
+
+
+bot.on('callback_query', (query) => {
+
 });
