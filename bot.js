ca1acdd0959ddd7fb953e1d64d2e96a9ca2897cf
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-06 21:59:57 +0200

    add new features (without bugs)

--- bot.js
+++ bot.js
@@ -24,8 +24,8 @@ const axiosCreateChat = async (name, chatid, email, password) => {
     return data;
   }
 };
-const axiosDeleteChat = async (name, email, password) => {
-  const { data } = await axios.post('api/tg/delete', { name, email, password });
+const axiosDeleteChat = async (name, deleteName) => {
+  const { data } = await axios.post('api/tg/delete', { chatid: String(name), deleteName });
   if (data) {
     return data;
   }
@@ -35,7 +35,7 @@ const bot = new TelegramApi(token, { polling: true });
 const buttonForm = {
   reply_markup: {
     keyboard: [
-      [{ text: 'Чаты поддержки' }, { text: 'Чаты сделок' }, { text: 'Мой ид' }, { text: "Вход" }],
+      [{ text: 'Чаты поддержки' }, { text: 'Чаты сделок' }, { text: 'Мой ид' }, { text: "Вход" }, { text: "Доступ" }],
     ],
     resize_keyboard: true
   }
@@ -76,6 +76,21 @@ ${item.name} ${item.chatid}`, '')}`)
       return bot.sendMessage(chatId, `Пошел нахуй чмо без доступа`)
     }
   }
+  if (ctx.text === 'Доступ') {
+    bot.sendMessage(chatId, `Ожидайте, выполняется запрос`)
+    const result = await axiosGetChatID();
+    const checkAccess = result.filter(item => item.chatid === String(chatId))[0]
+    if (checkAccess) {
+      return bot.sendMessage(chatId, 'Доступ:', {
+        reply_markup: {
+          inline_keyboard: result.map(item => [{ text: `${item.name} ${item.chatid}`, callback_data: `/deleteuser ${item.name}` }]),
+          resize_keyboard: true
+        }
+      })
+    } else {
+      return bot.sendMessage(chatId, `Пошел нахуй чмо без доступа`)
+    }
+  }
   if (ctx.text === "Вход") {
     bot.sendMessage(chatId, "Введите имя для записи в базу данных:");
     return bot.once('message', async (nameMsq) => {
@@ -106,5 +121,36 @@ ${item.name} ${item.chatid}`, '')}`)
 
 
 bot.on('callback_query', (query) => {
-
+  const chatId = query.from?.id
+  const data = query.data;
+  if (data.includes('deleteuser')) {
+    bot.sendMessage(chatId, "Вы уверены?", {
+      reply_markup: {
+        inline_keyboard: [[{ text: `Да`, callback_data: `Да` }, { text: `Нет`, callback_data: `Нет` }]],
+        resize_keyboard: true
+      }
+    });
+    return bot.once('callback_query', async (check) => {
+      const answer = check.data;
+      if (answer === 'Да') {
+        bot.sendMessage(chatId, `Ожидайте, выполняется запрос`);
+        const deleteUser = data.split(' ')[1]
+        if (deleteUser) {
+          try {
+            const deleteCheck = await axiosDeleteChat(chatId, deleteUser);
+            if (deleteCheck) {
+              return bot.sendMessage(chatId, `Успешно удалено`)
+            }
+          } catch (e) {
+            console.log('error', e?.response?.data)
+            bot.sendMessage(chatId, `Пошел нахуй, ${e.data.message.toLowerCase()}`)
+          };
+        } else {
+          return bot.sendMessage(chatId, `Имя не найдено(перезапустите бота)`);
+        }
+      } else {
+        return bot.sendMessage(chatId, `Отменено`);
+      }
+    })
+  }
 });
