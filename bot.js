3de9ec49987dfa2c572dbf754a54dd792a3bb387
Dima Lobanov <dmytro.lobanov@nure.ua>
2023-03-06 19:23:12 +0200

    fix

--- bot.js
+++ bot.js
@@ -13,9 +13,12 @@ axios.defaults.baseURL = `https://back-yipq.onrender.com`;
 const axiosGetChatID = async () => {
   const { data } = await axios.get('api/tg/get');
   if (data.telegramUsers) {
+    console.log(data.telegramUsers)
     return data.telegramUsers;
   }
 };
+
+
 const axiosCreateChat = async (name, chatid, email, password) => {
   const { data } = await axios.post('api/tg/create', { name, chatid, email, password });
   if (data) {
@@ -30,11 +33,25 @@ const axiosDeleteChat = async (name, email, password) => {
 };
 const token = '6149778778:AAFPlXbrTIXD_zDG4LLaeeqTH8fbKJM7kmA';
 const bot = new TelegramApi(token, { polling: true });
+const buttonForm = {
+  reply_markup: JSON.stringify({
+    inline_keyboard:[
+      [ {text: 'Чаты поддержки', callback_data: 'sdadas'} ],
+      [ {text: 'Чаты сделок', callback_data: 'eqweqwewq'} ],
+    ]
+  })
+}
+
+bot.setMyCommands([
+  {command: '/start', description: 'Запустить или перезапустить бота'},
+  {command: '/id', description: 'Получить id'},
+  {command: '/dostup', description: 'Функционал'},
+])
 
 bot.on('message', async (ctx) => {
   const chatId = ctx.from?.id;
-  if (ctx.text === '/MyId') {
-    return bot.sendMessage(chatId, `${chatId}`);
+  if (ctx.text === '/id') {
+    return bot.sendMessage(chatId, `твой id чата ${chatId}`);
   }
   if (ctx.text === 'Шо') {
     return bot.sendMessage(chatId, "Шо ты хуесос", {
@@ -50,14 +67,20 @@ bot.on('message', async (ctx) => {
   if (ctx.text === '/start') {
     return bot.sendMessage(chatId, "Для входа введите свой логин и пароль", {
       reply_markup: {
-        inline_keyboard: [
+        keyboard: [
           [{ text: "Вход", callback_data: "/login" }]
         ],
-        resize_keyboard: true
+        
       }
     });
   }
+  if (ctx.text === '/dostup') {
+    return bot.sendMessage(chatId, "Меню", buttonForm)
+  }
 });
+
+
+
 
 bot.on('callback_query', (query) => {
   const chatId = query.from?.id
@@ -79,12 +102,13 @@ bot.on('callback_query', (query) => {
         bot.once('message', async (passwordMsg) => {
           const password = passwordMsg.text;
           bot.sendMessage(chatId, `Ожидайте, выполняется запрос`);
-          console.log(2, name, chatId, login, password)
           try {
             const response = await axiosCreateChat(name, String(chatId), login, password)
+            axiosGetChatID()
             return bot.sendMessage(chatId, `Успешно`);
           } catch (e) {
             bot.sendMessage(chatId, `Пошел нахуй, ${e.response.data.message.toLowerCase()}`)
+            axiosGetChatID()
           }
         });
       });
